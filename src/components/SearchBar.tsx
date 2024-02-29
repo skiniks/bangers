a2bfbb972e404fdb21d1378b4960184b8476d699
Ryan Skinner <ryanskinner@gmail.com>
2024-02-29 00:06:32 -0500

    tightening up spacing

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -18,7 +18,7 @@ const SearchBar: React.FC<SearchBarProps> = ({
         value={tempIdentifier}
         onChange={e => setTempIdentifier(e.target.value)}
         placeholder="Enter user handle"
-        className="w-full mb-4 form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent text-gray-800"
+        className="w-full mb-2 form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent text-gray-800"
       />
       <button
         onClick={fetchPosts}
