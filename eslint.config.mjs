7661b9696865408eb012a597a05ad42b2bd706b6
Ryan Skinner <hello@ryanskinner.com>
2024-11-29 00:15:26 -0500

    turning rule off

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,3 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({ react: true }, { rules: { 'perfectionist/sort-imports': 'off' } })
+export default antfu({ react: true }, { rules: { 'perfectionist/sort-imports': 'off', 'style/indent': 'off' } })
