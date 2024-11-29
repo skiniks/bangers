6200731bc5ab99ad1f38c7e78ae62ab03d3638ce
Ryan Skinner <hello@ryanskinner.com>
2024-11-29 03:17:48 -0500

    remove defaults

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,17 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            refetchOnWindowFocus: false,
-            retry: 1,
-          },
-        },
-      }),
-  )
+  const [queryClient] = useState(() => new QueryClient())
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
