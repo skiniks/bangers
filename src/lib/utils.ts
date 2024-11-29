3a590b8573ce1cb16d0037bd81c55001c0bd84fe
Ryan Skinner <hello@ryanskinner.com>
2024-11-28 23:06:59 -0500

    fix type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-export function getElapsedTime(createdAt: string) {
+export function getElapsedTime(createdAt: string | number | Date) {
   const currentTime = new Date()
   const createdAtTime = new Date(createdAt)
   const elapsedTime = currentTime.getTime() - createdAtTime.getTime()
